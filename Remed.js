e219e5f82fda3195978fb76166aa11a6e502fb6a
unknown <PugarHuda>
2022-03-31 20:23:29 +0700

    remed

---
+++ Remed.js
@@ -0,0 +1,6 @@
+function countUnique(iterable) {
+    return new Set([1, 1, 1, 1, 1, 2, 2, 2, 2, 2]).length;
+}
+// console.log(countUnique([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+// console.log(countUnique([1, 2, 2, 2, 2, 2, 2, 2, 4, 6]));
+console.log(countUnique([]));
